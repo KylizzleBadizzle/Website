765db4fa4800a260d5fe20cfbda42cfa1102b635
KylizzleBadizzle <Nova.afzelius@gmail.com>
2016-08-09 20:27:12 +0100

    New Source Code
    webside

---
+++ New Text Document.cs
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <meta http-equiv="X-UA-Compatible" content="IE=9,chrome=1"/>
+    <meta charset="utf-8"/>
+<title>pokeaccs</title>
+<meta name="fb_admins_meta_tag" content=""/>
+<meta name="description" content="Main Menu"/>
+<link rel="shortcut icon" href="http://www.wix.com/favicon.ico" type="image/x-icon"/>
+<link rel="apple-touch-icon" href="http://www.wix.com/favicon.ico" type="image/x-icon"/>
+    <meta http-equiv="X-Wix-Renderer-Server" content="app211.vae.aws"/>
+<meta http-equiv="X-Wix-Meta-Site-Id" content="4bb3aa8e-34aa-461c-a123-702f93628761"/>
+<meta http-equiv="X-Wix-Application-Instance-Id" content="dae0e2cb-7f26-4636-94c1-fbf13aa47665"/>
+<meta http-equiv="X-Wix-Published-Version" content="5"/>
+
+<meta http-equiv="etag" content="e6f4c2286754c502317e38e907089af2"/>
+<meta property="og:title" content="pokeaccs"/>
+<meta property="og:type" content="article"/>
+<meta property="og:url" content="http://novaafzelius.wixsite.com/pokeaccs"/>
+<meta property="og:site_name" content="pokeaccs"/>
+<meta property="og:description" content="Main Menu"/>
+<meta name="SKYPE_TOOLBAR" content="SKYPE_TOOLBAR_PARSER_COMPATIBLE"/>
+
+<meta id="wixMobileViewport" name="viewport" content="minimum-scale=0.25, maximum-scale=1.2"/>
+
+
+        
+    </head>
+<body>
+    
+        <div class="WixHeader">
+        <a href="http://sv.wix.com/lpviral/sv-hemsida?utm_campaign=vir_wixad_live&experiment_id=abtestbanner122400001" rel="nofollow"><span class="smallMusa">(Wix-Logo) </span>Skapa en <span class="smallLogo">Wix</span>-hemsida!</a> <br/>
+        <a href="http://sv.wix.com/lpviral/sv-hemsida?utm_campaign=vir_wixad_live&experiment_id=abtestbanner122400001" rel="nofollow">Hundratals mallar<br />Ingen kodning krävs<br /><span class="emphasis spacer">Börja nu >></span></a>
+    </div> 
+    
+            <header class="container" id="SITE_HEADER" >
+            <div id="i4b7xq1e" type="TPA-Part" class="anon$2">
+<IFRAME src="http://ecom.wix.com/storefront/cartwidget?instance=FLC6gSqkCaSkW3hHtbNuRNnyEYSUYMYpQpInf3JAJUk.eyJpbnN0YW5jZUlkIjoiM2QyMjI4ZjAtMWU0OS00MDJlLTk4OTItM2FiZTc4MGE0Nzg1Iiwic2lnbkRhdGUiOiIyMDE2LTA4LTA5VDE2OjI1OjQ0LjcxNFoiLCJ1aWQiOm51bGwsInZlbmRvclByb2R1Y3RJZCI6bnVsbCwiZGVtb01vZGUiOmZhbHNlfQ&target=_top&width=49&compId=i4b7xq1e&viewMode=viewer-seo&section-url=http%3A%2F%2Fnovaafzelius.wixsite.com%2Fpokeaccs%2Fproduct-page" width="49" scrolling="auto" frameborder="0"></IFRAME>
+</div>
+            <div id="i4b1y4r7" itemscope itemtype="http://schema.org/ImageObject" class="image">
+
+        <a href="http://novaafzelius.wixsite.com/pokeaccs" target="_self">
+            <img src="https://static.wixstatic.com/media/a9ff3b_bde0e140d0bf4142a0dd482a747fed15.png_srz_110_23_85_22_0.50_1.20_0.00_png_srz" alt="" itemprop="contentURL" width="110" height="23"/>
+        </a>
+    </div>
+
+            <div id="i4b1w9x7" class="Text">
+    <h1 class="font_0" style="line-height: 1.3em; text-align: center; font-size: 31px;"><span style="font-family:verdana,geneva,sans-serif;"><span style="font-size:31px;"><span style="color:#000000;"><a href="http://novaafzelius.wixsite.com/pokeaccs" target="_self">Pokemon GO accounts boosting &amp; More!</a></span></span></span></h1>
+
+<h1 class="font_0" style="line-height: 1.3em; text-align: center;"><span style="color:#000000;"><span style="font-family:reklamescriptw00-medium,cursive;"><span style="text-shadow:#c8c8c8 1px 1px 0px, #b4b4b4 0px 2px 0px, #a0a0a0 0px 3px 0px, rgba(140, 140, 140, 0.498039) 0px 4px 0px, #787878 0px 0px 0px, rgba(0, 0, 0, 0.498039) 0px 5px 10px;"><a href="http://novaafzelius.wixsite.com/pokeaccs" target="_self">More products incomming! check  "more" tab!</a></span></span></span></h1>
+
+</div>
+
+
+            <nav>
+    <ul id="i4b27reb" class="menu">
+                <li class="menuItem">
+    <a id="am8" href="http://novaafzelius.wixsite.com/pokeaccs" target="_self" title="Main Menu" class="menuItemHref">Main Menu</a>
+    </li>
+
+                <li class="menuItem">
+    <a id="1u5p" href="http://novaafzelius.wixsite.com/pokeaccs/shop" target="_self" title="Shop" class="menuItemHref">Shop</a>
+    </li>
+
+                <li class="menuItem">
+    <a id="1sbg" href="http://novaafzelius.wixsite.com/pokeaccs/about" target="_self" title="About" class="menuItemHref">About</a>
+    </li>
+
+                <li class="menuItem">
+    <a id="11ge" href="http://novaafzelius.wixsite.com/pokeaccs/faq" target="_self" title="FAQ" class="menuItemHref">FAQ</a>
+    </li>
+
+                <li class="menuItem">
+    <a id="od3" href="http://novaafzelius.wixsite.com/pokeaccs/contact" target="_self" title="Contact" class="menuItemHref">Contact</a>
+    </li>
+
+            </ul>
+</nav>
+
+    </header>
+<section class="page" id="c1dmp">
+                <ul id="i4b2sxm5" class="Gallery" type="wysiwyg.viewer.components.SlideShowGallery">
+    <li id="dataItem-irni9vlu" itemscope itemtype="http://schema.org/ImageObject">
+            <a href="http://novaafzelius.wixsite.com/pokeaccs?lightbox=dataItem-irni9vlu">
+                    <img src="https://static.wixstatic.com/media/85dec6_f8ce6034adfd4f939cb8c6a444418992%7Emv2.jpg_srz_980_490_85_22_0.50_1.20_0.00_jpg_srz" alt="Pokemon-Go-Characters" itemprop="contentURL" width="980" height="490"/>
+    </a>    
+            <p itemprop="name">Pokemon-Go-Characters</p>
+        </li>
+
+    <li id="dataItem-irni9vlw" itemscope itemtype="http://schema.org/ImageObject">
+            <a href="http://novaafzelius.wixsite.com/pokeaccs?lightbox=dataItem-irni9vlw">
+                    <img src="https://static.wixstatic.com/media/85dec6_3ccaaceac60242eaa439029c56b33b90%7Emv2.jpg_srz_980_551_85_22_0.50_1.20_0.00_jpg_srz" alt="vmY4Y4tMmmGP3K26GcnPMU-970-80" itemprop="contentURL" width="980" height="551"/>
+    </a>    
+            <p itemprop="name">vmY4Y4tMmmGP3K26GcnPMU-970-80</p>
+        </li>
+
+    <li id="dataItem-irni9vlw1" itemscope itemtype="http://schema.org/ImageObject">
+            <a href="http://novaafzelius.wixsite.com/pokeaccs?lightbox=dataItem-irni9vlw1">
+                    <img src="https://static.wixstatic.com/media/85dec6_9832b67d69764e958255028e77e8e9f5%7Emv2.jpg_srz_980_544_85_22_0.50_1.20_0.00_jpg_srz" alt="pokemon-go-ebay.jpg.optimal" itemprop="contentURL" width="980" height="544"/>
+    </a>    
+            <p itemprop="name">pokemon-go-ebay.jpg.optimal</p>
+        </li>
+
+    <li id="dataItem-irni9vlx" itemscope itemtype="http://schema.org/ImageObject">
+            <a href="http://novaafzelius.wixsite.com/pokeaccs?lightbox=dataItem-irni9vlx">
+                    <img src="https://static.wixstatic.com/media/85dec6_04f429c3ef764ac1bfc63e51ee96a80c%7Emv2.jpg_srz_980_512_85_22_0.50_1.20_0.00_jpg_srz" alt="https-__blueprint-api-production.s3.amazonaws.com_uploads_story_thumbnail_14737_Pokemon_Go_level30_h" itemprop="contentURL" width="980" height="512"/>
+    </a>    
+            <p itemprop="name">https-__blueprint-api-production.s3.amazonaws.com_uploads_story_thumbnail_14737_Pokemon_Go_level30_h</p>
+        </li>
+
+</ul>
+    </section>
+<footer class="container" id="SITE_FOOTER" >
+            <ul id="i4b2oz7j" class="SocialLinkBar">
+    <li itemscope itemtype="http://schema.org/ImageObject" class="LinkBarItem">
+    <a href="https://www.facebook.com/groups/Pokeaccs/">
+        <img src="https://static.wixstatic.com/media/0fdef751204647a3bbd7eaa2827ed4f9.png" itemprop="contentURL" width="200" height="200"/>
+    </a>
+</li>
+
+    <li itemscope itemtype="http://schema.org/ImageObject" class="LinkBarItem">
+    <a href="http://instagram.com/scrubbydooo">
+        <img src="https://static.wixstatic.com/media/01c3aff52f2a4dffa526d7a9843d46ea.png" itemprop="contentURL" width="200" height="200"/>
+    </a>
+</li>
+
+</ul>
+    </footer>
+
+    
+        
+        <div class="adFootBox" style="background-color:black">
+        <div class="adFootBox"><div class="siteBanner" ><div class="siteBanner"><div class="wrapper"><div class="bigMusa">(Wix Logo)</div><div class="txt shd" style="color:#fff">Denna hemsida skapades med </div> <div class="txt shd"><a  href="http://sv.wix.com?utm_campaign=vir_wixad_live&experiment_id=abtestbanner122400001" style="color:#fff"> WIX.com. </a></div> <div class="txt shd" style="color:#fff"> Skapa din egen helt GRATIS <span class="emphasis"> >></span></div></div></div></div></div>
+    </div>
+</body>
+</html>
